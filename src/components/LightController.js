b1cb3d2913b93d919e3d4da34cf6ecc89b54186f
Mico Piira <mico.piira@gmail.com>
2019-11-19 23:41:33 +0200

    Extract a reusable LightListItem component

--- src/components/LightController.js
+++ src/components/LightController.js
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react';
 import ColorWheel from "./ColorWheel";
 import {Link, useParams} from 'react-router-dom';
-import {cie_to_rgb, rgb_to_cie} from "../cie_rgb_converter";
-import CustomSwitch from "./bootstrap/CustomSwitch";
+import {rgb_to_cie} from "../cie_rgb_converter";
 import useLight from "../hooks/useLight";
-import CustomRange from "./bootstrap/CustomRange";
 import ApiContext from "./ApiContext";
 import Octicon, {ChevronLeft} from '@primer/octicons-react';
+import LightListItem from "./LightListItem";
 
 function LightController() {
 	const { api } = useContext(ApiContext);
@@ -19,31 +18,11 @@ function LightController() {
 	};
 
 	const setLightState = newState => api.setLightState({lightId, newState}).then(() => fetchLight());
-	const backgroundColor = light ? (() => {
-		if (!light.state.on) return 'rgb(90, 90, 90)';
-		return light.state.xy ? `rgb(${cie_to_rgb(light.state.xy[0], light.state.xy[1]).join(',')})` : 'rgb(255,255,255)'
-	})() : '';
+
 	return (<React.Fragment>
-		<div style={{backgroundColor}}>
-			<div className="container p-2 mb-4">
-				<div className="row mb-1">
-					<div className="col-auto">
-						<Link to="/"><Octicon icon={ChevronLeft}/></Link>
-					</div>
-					<div className="col"><strong>{light ? light.name : 'Loading...'}</strong></div>
-					<div className="col-auto">
-						{light && <CustomSwitch id={`customSwitch${lightId}`} checked={light.state.on}
-												onChange={() => setLightState({on: !light.state.on})}/>}
-					</div>
-				</div>
-				<div className="row">
-					<div className="col">
-						{light ? <CustomRange defaultValue={light.state.bri} min={0} max={254}
-											  onChange={e => setLightState({bri: parseInt(e.target.value)})}/> : ''}
-					</div>
-				</div>
-			</div>
-		</div>
+		{light && <LightListItem light={light} lightId={lightId} setLightState={setLightState} icon={
+			<Link to="/" className="text-dark"><Octicon icon={ChevronLeft} size='medium' verticalAlign='middle'/></Link>
+		}/>}
 		<div className="container">
 			<ColorWheel onColorClick={handleColorChange}/>
 		</div>
--- src/components/LightList.js
+++ src/components/LightList.js
@@ -1,13 +1,10 @@
 import React, {useContext} from 'react';
 import BulbIcon from './BulbIcon';
-import {cie_to_rgb} from "../cie_rgb_converter";
-import {Link} from "react-router-dom";
 import Nav from "./layout/Nav";
-import CustomSwitch from "./bootstrap/CustomSwitch";
 import useLights from "../hooks/useLights";
-import CustomRange from "./bootstrap/CustomRange";
 import ApiContext from "./ApiContext";
 import Spinner from "./bootstrap/Spinner";
+import LightListItem from "./LightListItem";
 
 const archeTypeAliases = {
 	sultanbulb: 'BulbsSultan',
@@ -32,40 +29,12 @@ function LightList() {
 			<Nav title="Hue online"/>
 			<div className="container">
 				{Object.entries(lights).map(([lightId, light]) => {
-					const backgroundColor = (() => {
-						if (!light.state.on) return 'rgb(90, 90, 90)';
-						return light.state.xy ? `rgb(${cie_to_rgb(light.state.xy[0], light.state.xy[1]).join(',')})` : 'rgb(255,255,255)'
-					})();
 					return <React.Suspense fallback={<Spinner/>} key={light.uniqueid}>
-						<div className={['card mb-2'].concat(light.state.on ? 'text-dark' : 'text-light').join(' ')}
-							 style={{backgroundColor}}>
-							<div className="card-body"
-								 style={light.state.on && light.state.bri ? {paddingBottom: 0} : {}}>
-								<div className="row justify-content-center">
-									<div className="col-auto">
-										<BulbIcon width="1.5rem" height="1.5rem"
-												  icon={archeTypeAliases[light.config.archetype]}/>
-									</div>
-									<div className="col">
-										<strong>{light.name}</strong>
-									</div>
-									<div className="col-3" style={{zIndex: 2}}>
-										<div className="float-right">
-											<CustomSwitch id={`customSwitch${lightId}`} checked={light.state.on}
-														  onChange={() => setLightState(lightId, {on: !light.state.on})}/>
-										</div>
-									</div>
-								</div>
-								{light.state.on && light.state.bri && <div className="row">
-									<div className="col" style={{zIndex: 2}}>
-										<CustomRange defaultValue={light.state.bri} min={0} max={254}
-													 onChange={e => setLightState(lightId, {bri: parseInt(e.target.value)})}/>
-									</div>
-								</div>}
-								{light.capabilities.control.colorgamuttype &&
-								<Link to={`/${lightId}`} className="stretched-link"/>}
-							</div>
-						</div>
+						<LightListItem light={light}
+									   lightId={lightId}
+									   setLightState={newState => setLightState(lightId, newState)}
+									   stretchedLink={`/${lightId}`}
+									   icon={<BulbIcon width="1.5rem" height="1.5rem" icon={archeTypeAliases[light.config.archetype]}/>}/>
 					</React.Suspense>
 				})}
 			</div>
