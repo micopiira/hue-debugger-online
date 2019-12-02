55741744eaf6302c31c06eaeeeebb374f6a909f2
Mico Piira <mico.piira@gmail.com>
2019-12-02 20:59:44 +0200

    Rebuild hue icons

--- src/components/HueIconPack2019/ZonesAreasSecondfloor.js
+++ src/components/HueIconPack2019/ZonesAreasSecondfloor.js
@@ -2,8 +2,7 @@ import React from "react";
 
 const SvgZonesAreasSecondfloor = props => (
 	<svg viewBox="0 0 32 32" width="1em" height="1em" {...props}>
-		<path
-			d="M27.66 14.19L16.71 4.28a1 1 0 00-1.42 0L4.34 14.19A1 1 0 005.05 16H7v11a1 1 0 001 1h16a1 1 0 001-1V16h2a1 1 0 00.66-1.81zM7.53 14L16 6.33 24.47 14z"/>
+		<path d="M27.66 14.19L16.71 4.28a1 1 0 00-1.42 0L4.34 14.19A1 1 0 005.05 16H7v11a1 1 0 001 1h16a1 1 0 001-1V16h2a1 1 0 00.66-1.81zM7.53 14L16 6.33 24.47 14z" />
 	</svg>
 );
 
