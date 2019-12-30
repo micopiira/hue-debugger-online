86f455414d7239c79788e9158b4ef832b1146eaa
Mico Piira <mico.piira@gmail.com>
2019-12-30 13:25:38 +0200

    Make linking process more automatic
    Now polls the bridge every 1 second for 30 seconds
    instead of manually needing to press the button

--- src/components/LinkBridge.js
+++ src/components/LinkBridge.js
@@ -1,19 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {handleJsonResponse} from "../fetchUtils";
+import Octicon, {Alert} from '@primer/octicons-react';
+
+const TOTAL_TIME_SECONDS = 30;
 
 function LinkBridge({bridge, setUsername}) {
 
 	const [error, setError] = useState(null);
+	const [progress, setProgress] = useState(TOTAL_TIME_SECONDS);
 
-	const link = () => fetch(bridge + '/api', {method: 'POST', body: JSON.stringify({devicetype: 'hueonline'})})
-		.then(handleJsonResponse)
-		.then(json => json[0])
-		.then(json => json.error ? Promise.reject(new Error(json.error.description)) : json)
-		.then(json => setUsername(json.success.username))
-		.catch(error => {
-			console.error(error);
-			setError(error.message);
-		});
+	const startInterval = useCallback(() => {
+		if (progress > 0) {
+			setTimeout(() => {
+				fetch(bridge + '/api', {method: 'POST', body: JSON.stringify({devicetype: 'hueonline'})})
+					.then(handleJsonResponse)
+					.then(json => json[0])
+					.then(json => json.error ? Promise.reject(new Error(json.error.description)) : json)
+					.then(json => setUsername(json.success.username))
+					.catch(error => {
+						if (error.message === 'link button not pressed') {
+							setProgress(prevState => prevState - 1);
+						} else {
+							console.error(error.message);
+							setError(error.message);
+						}
+					});
+			}, 1000);
+		}
+	}, [bridge, setUsername, setProgress, progress]);
+
+	useEffect(() => {
+		startInterval();
+	}, [startInterval]);
 
 	return <div className="container pt-2">
 		{error && <div className="alert alert-danger" role="alert">
@@ -23,8 +41,13 @@ function LinkBridge({bridge, setUsername}) {
 			</button>
 		</div>}
 		<div className="text-center">
-		<p className="lead">Press the button on bridge {bridge}, then click "Link" button below.</p>
-		<button onClick={() => link()} type="button" className="btn btn-primary btn-lg">Link</button>
+		<p className="lead">Press the link button on bridge {bridge}...</p>
+			{progress === 0 ? <button className="btn btn-primary" onClick={() => setProgress(TOTAL_TIME_SECONDS)}><Octicon icon={Alert}/> Retry</button> :
+				<div className="progress">
+					<div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar"
+						 aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: progress * (100 / TOTAL_TIME_SECONDS) + '%'}}/>
+				</div>
+			}
 		</div>
 	</div>
 }
