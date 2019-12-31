faa40b7d557069c1becf131caff64a4b799a8403
Mico Piira <mico.piira@gmail.com>
2019-12-31 14:26:33 +0200

    Use suspense api for fetching bridges

--- src/components/BridgeSetup.js
+++ src/components/BridgeSetup.js
@@ -1,27 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import hue from 'hue-api';
+import {wrapPromise} from "../wrapPromise";
 
 function BridgeSetup({setBridge}) {
-	const [bridges, setBridges] = useState([]);
 
-	useEffect(() => {
-		hue().bridgeDiscovery.nupnpScan()
+	const [resource, setResource] = useState(null);
+
+	const fetchBridges = useCallback(() => {
+		setResource(wrapPromise(hue().bridgeDiscovery.nupnpScan()
 			.then(results =>
-				setBridges(results
+				results
 					.map(result => result.internalipaddress)
-					.map(ip => ip.startsWith('http') ? ip : window.location.protocol + '//' + ip))
-			)
+					.map(ip => ip.startsWith('http') ? ip : window.location.protocol + '//' + ip)
+			)));
 	}, []);
 
-	return bridges ? <div className="container">
-		<h1>Select bridge:</h1>
+	useEffect(() => {
+		fetchBridges();
+	}, [fetchBridges]);
+
+	if (!resource) return null;
+
+	const bridges = resource.read();
+
+	return <div className="container">
+		{bridges.length > 0 ? <><h1>Select bridge:</h1>
 		<div className="list-group">
 		{bridges.map(bridge =>
 			<button className="list-group-item list-group-item-action" key={bridge} onClick={() => setBridge(bridge)}>{bridge}</button>
 		)}
-		</div>
-	</div> : <p>Could not find any bridges from your local network using N-UPnP scan. Give bridge IP Address manually:</p>;
-
+		</div></>
+			: <p>No bridges found <button className="btn btn-primary" onClick={fetchBridges}>Re-fetch</button></p>}
+	</div>;
 }
 
 export default BridgeSetup;
