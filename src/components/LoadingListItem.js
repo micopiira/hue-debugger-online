66c971ea53d8e7c6b16081a00ae48daca52f518d
Mico Piira <mico.piira@disec.fi>
2020-06-09 20:42:36 +0300

    Loading list item style changes

--- src/components/LoadingListItem.js
+++ src/components/LoadingListItem.js
@@ -3,12 +3,19 @@ import CustomSwitch from "./bootstrap/CustomSwitch";
 
 function LoadingListItem() {
 	return (
-		<div className="card mb-2 text-light flex-fill">
+		<div className="card mb-2 text-light flex-fill" style={{backgroundColor: 'rgb(90, 90, 90)'}}>
 			<div className="card-body">
 				<div className="row align-items-center mb-1">
-					<div className="col-auto">...</div>
+					<div className="col-auto"/>
 					<div className="col">
-						<strong>...</strong>
+						<div className="progress">
+							<div className="progress-bar progress-bar-striped progress-bar-animated"
+								 role="progressbar"
+								 aria-valuenow={100}
+								 aria-valuemin="0"
+								 aria-valuemax="100"
+								 style={{width: 100 + '%', backgroundColor: '#6a6a6a'}}/>
+						</div>
 					</div>
 					<div className="col-3" style={{zIndex: 2}}>
 						<div className="float-right">
